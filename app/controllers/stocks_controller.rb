73f51b5a225f4e4827479018443f9ddc0bdc3a1f
Ebrahim Farhangdoust <fhg.ebrahim@gmail.com>
2025-03-02 10:42:44 -0500

    param

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,6 @@
 class StocksController < ApplicationController
   def show
+    binding.break
     @stock = Stock.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "stocks/show"
+  get "stocks/show/:id" => "stocks#show", as: :stocks_show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
